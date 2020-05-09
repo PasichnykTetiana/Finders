552bb20547651f9841b600f77fe74635d3650c70
Tetiana <t.pasichnyk@ukr.net>
2020-05-10 02:04:33 +0300

    2

--- Finders.js
+++ Finders.js
@@ -1,7 +1,17 @@
 function findElement(arr, func) {
-    let num = 0;
+    // let num = 0;
+    var num;
+    var i;
+    for(i=0; i<arr.length; i+=1){
+        num=arr[i];
+        // if(num % 2 === 0){
+        //     console.log(num)
+        // }
+        console.log(func);
+    }
     return num;
     //найти первый элемент что делится без остатка
 }
 
-findElement([1, 2, 3, 4], num => num % 2 === 0);
+// findElement([1, 2, 3, 4], num => num % 2 === 0);
+findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; });
